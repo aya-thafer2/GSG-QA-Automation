deac7f456bcf541de08684efe4d12c6a16183875
aya-thafer2 <ayathafer2@gmail.com>
2023-10-14 19:27:48 +0300

    OrangeHRM-Task1

--- cypress/support/pageObjects/OrangeHRM/Recruitment/Candidates.ts
+++ cypress/support/pageObjects/OrangeHRM/Recruitment/Candidates.ts
@@ -3,13 +3,6 @@ class Candidates {
     elements = {
         goToRecruitment: () => cy.get(':nth-child(5) > .oxd-main-menu-item'),
         table: () => cy.get('.oxd-table-body'),
-        scheduleInterviewBtn: () => cy.get('.oxd-button--success'),
-        interviewTitle: () => cy.get(':nth-child(2) > .oxd-grid-3 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input'),
-        interviewer: () => cy.get('[placeholder="Type for hints..."]'),
-        selectInterviewer: ()=> cy.get('.oxd-autocomplete-dropdown > :nth-child(1)'),
-        Date: () => cy.get('.oxd-date-input > .oxd-icon'),
-        selectDate: ()=>cy.get(':nth-child(12) > .oxd-calendar-date'),
-        saveBtn: () => cy.get('.oxd-button--secondary')
     }
 
     goToRecruitment() {
@@ -19,23 +12,7 @@ class Candidates {
     getRecords(APItotal: number) {
         this.elements.table().find('.oxd-table-row').should('have.length', APItotal);
     }
-    clickScheduleInterviewBtn(){
-        this.elements.scheduleInterviewBtn().click({force: true})
-    }
-    clickSaveBtn(){
-        this.elements.saveBtn().click({force: true})
-    }
-    fillScheduleInterviewData(){
-        this.elements.interviewTitle().type('IDC')
-        this.elements.interviewer().type('s')
-        cy.wait(2000)
-        this.elements.selectInterviewer().click();
-        this.elements.Date().click();
-        this.elements.selectDate().click();
-        this.clickSaveBtn();
-        cy.wait(5000);
-        cy.contains('.orangehrm-recruitment-status > .oxd-text',"Status: Interview Scheduled").should("exist");
-    }
+   
 
 }
 export default Candidates;
