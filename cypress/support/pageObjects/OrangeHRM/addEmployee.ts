443cf40540aff5801d12c8f2146425d51a0a803d
aya-thafer2 <ayathafer2@gmail.com>
2023-10-27 12:07:12 +0300

    finished-Scenario #1

--- cypress/support/pageObjects/OrangeHRM/addEmployee.ts
+++ cypress/support/pageObjects/OrangeHRM/addEmployee.ts
@@ -19,7 +19,19 @@ class addEmployee {
   getEmpNumber(): number {
     return this.empNumber;
   }
-  
+  setUserName(userName: string) {
+    this.userName = userName;
+  }
+  getUserName(): string {
+    return this.userName;
+  }
+  setLeaveId(leaveId: number) {
+    this.leaveId = leaveId;
+  }
+  getLeaveId(): number {
+    return this.leaveId;
+  }
+
   elements = {
     MainMenuItems: () => cy.get('.oxd-sidepanel-body'),
     AddEmp: () => cy.get('.oxd-button--secondary'),
@@ -33,6 +45,14 @@ class addEmployee {
     editEmployeeName: () => cy.get('.orangehrm-edit-employee-name'),
     loader: () => cy.get('.oxd-loading-spinner'),
 
+    dropDown: () => cy.get('.oxd-userdropdown-tab > .oxd-icon'),
+    logout: () => cy.get(':nth-child(4) > .oxd-userdropdown-link'),
+
+    userNameEmp: () => cy.getByPlaceholder('Username'),
+    passwordEmp: () => cy.getByPlaceholder('Password'),
+    loginBtnEmp: () => cy.get('button'),
+
+    leaveTable: () => cy.get('.oxd-table-body')
   }
 
   selectPIM() {
@@ -62,6 +82,100 @@ class addEmployee {
         expect(response).property("status").to.equal(200);
         this.setEmpNumber(response.body?.data.empNumber);
       });
+    });
+  }
+
+  createLoginDetails(username: string, password: string): Cypress.Chainable<any> {
+    return cy.wrap(undefined).then(() => {
+      cy.request({
+        method: "POST",
+        url: "/web/index.php/api/v2/admin/users",
+        body: {
+          empNumber: this.getEmpNumber(),
+          password: password,
+          status: true,
+          userRoleId: 2,
+          username: username
+        },
+      }).then((response) => {
+        console.log(response.body.data.userName);
+        this.setUserName(response.body.data.userName);
+        expect(response).property("status").to.equal(200);
+      });
+    });
+  }
+
+  logout() {
+    this.elements.dropDown().click()
+    this.elements.logout().click()
+  }
+
+  addLeaveEntitlement() {
+    cy.request({
+      method: "POST",
+      url: `/web/index.php/api/v2/leave/leave-entitlements`,
+      body:{
+        empNumber: this.getEmpNumber(), 
+        leaveTypeId: 8, 
+        fromDate: "2023-01-01", 
+        toDate: "2023-12-31", 
+        entitlement: "40"
+      }
+      
+    }).then((response) => {
+      expect(response).property("status").to.equal(200);
+    });
+  }
+  
+  applyLeave(){
+    cy.request({
+      method: "POST",
+      url: `/web/index.php/api/v2/leave/leave-requests`,
+      body:{
+        leaveTypeId: 8, 
+        fromDate: "2023-10-30", 
+        toDate: "2023-10-30", 
+        comment: null
+      }
+      
+    }).then((response) => {
+      console.log(response.body.data.id);
+      this.setLeaveId(response.body.data.id);
+      expect(response).property("status").to.equal(200);
+    });
+  }
+
+  adminApprovesLeave(){
+    cy.request({
+      method: "PUT",
+      url: `/web/index.php/api/v2/leave/employees/leave-requests/${this.getLeaveId()}`,
+      body:{
+        action: "APPROVE"
+      }
+      
+    }).then((response) => {
+      console.log(response);
+      expect(response).property("status").to.equal(200);
+    });
+  }
+
+  selectLeave() {
+    this.elements.MainMenuItems().contains('Leave').click();
+  }
+
+  leaveAssertion(){
+    cy.contains('.oxd-table-card > .oxd-table-row', 'Scheduled').should("exist");
+  }
+
+  deleteEmployee(){
+    cy.request({
+      method: "DELETE",
+      url: "/web/index.php/api/v2/pim/employees",
+      body: {
+        ids: [this.getEmpNumber()],
+      }
+    }).then((response) => {
+      expect(response).property("status").to.equal(200);
     });
   }
 }
