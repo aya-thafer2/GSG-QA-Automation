f0a688824624588a1a2ce01ba1f5d92d3663b546
aya-thafer2 <ayathafer2@gmail.com>
2023-10-28 21:14:18 +0200

    TimeSheet Task

--- cypress/e2e/tests/OrangeHRM/Time/TimeSheets.spec.cy.ts
+++ cypress/e2e/tests/OrangeHRM/Time/TimeSheets.spec.cy.ts
@@ -1,12 +1,8 @@
 import addNewEmployee from "../../../../support/helpers/OrangeHRM/EmployeeHelper";
 import LoginPage from "../../../../support/pageObjects/OrangeHRM/LoginPage";
 import addEmployee from "../../../../support/pageObjects/OrangeHRM/addEmployee";
-import employeePersonalInfo from "../../../../support/pageObjects/OrangeHRM/employeePersonalInfo";
-import employeeTable from "../../../../support/pageObjects/OrangeHRM/employeeTable";
 const LoginObj: LoginPage = new LoginPage();
 const addEmployeeObj: addEmployee = new addEmployee();
-const employeePersonalInfoObj: employeePersonalInfo = new employeePersonalInfo();
-const employeeTableObj: employeeTable = new employeeTable();
 
 describe("Time: Create TimeSheets Process", () => {
     beforeEach(() => {
